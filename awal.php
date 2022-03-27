dbf5fd6adbcfc9386b43c84b1f3025b3b6d090cc
lumbangaolmarbun <102027928+lumbangaolmarbun@users.noreply.github.com>
2022-03-27 19:29:59 +0800

    Add files via upload

---
+++ awal.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>FORM UNTUK ENKRIPSI</title>
+</head>
+<body>
+<form action="enkcaesar.php" method="get">
+Plainteks : <input type="fransiska" name="kata"> <br>
+Key : <input type="az" name="key" maxlength="2"> <br>
+<input type="submit" value="kirim">
+<input type="reset" value="ulangi">
+</form>
+</body>
+</html>
